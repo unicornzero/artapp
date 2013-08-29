e602be0bc5cecf56ba67e2bd0be184b4ebdd6809
unicornzero <rylee.k@gmail.com>
2013-08-29 14:20:54 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Artapp::Application.config.secret_key_base = 'e707b2edcfa6d2ef80d956a1ad5916b4417162ec2df90a51bae27964e325fcfe006e32db756fed618ed8663c3f0fd360e60de837f2939f5bd89e722895d30acc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Artapp::Application.config.session_store :cookie_store, key: '_artapp_session'
