954b1953d62ce8f75b90262fffaddada363887bc
unicornzero <rylee.k@gmail.com>
2013-08-29 17:22:56 -0600

    first feature spec created

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'User management' do
+  scenario 'create a new user' do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'Email', with: 'user1@example.com'
+    fill_in 'Password', with: 'password1'
+    fill_in 'Confirm Password', with: 'password1'
+    click_button 'Create Your Account'
+    expect(page).to have_content 'Your account has been created.'
+  end
+end
