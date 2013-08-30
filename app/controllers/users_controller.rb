924cb750b4e48d25d06ddefdb07adf22a477921d
unicornzero <rylee.k@gmail.com>
2013-08-29 23:45:42 -0600

    user is logged in after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Your account has been created."
     else
       flash[:error] = "Please try again."
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -12,20 +12,21 @@ feature 'User management' do
     expect(page).to have_content 'user1@example.com'
   end
 
-  scenario 'log in' do
+  scenario 'signup logs user in' do
     visit root_path
     click_link 'Sign Up'
     fill_in 'Email', with: 'user1@example.com'
     fill_in 'Password', with: 'password1'
     fill_in 'Confirm Password', with: 'password1'
     click_button 'Create Your Account'
-
+=begin
     visit root_path
     click_link 'Log In'
     fill_in 'Email', with: 'user1@example.com'
     fill_in 'Password', with: 'password1'
     click_button 'Log In'
     expect(page).to have_content 'You are logged in.'
+=end
     expect(page).to have_content 'Log Out'
     expect(page).to_not have_content 'Log In'
     expect(page).to_not have_content 'Sign Up'
@@ -38,12 +39,13 @@ feature 'User management' do
     fill_in 'Password', with: 'password1'
     fill_in 'Confirm Password', with: 'password1'
     click_button 'Create Your Account'
-
+=begin
     visit root_path
     click_link 'Log In'
     fill_in 'Email', with: 'user1@example.com'
     fill_in 'Password', with: 'password1'
     click_button 'Log In'
+=end
     click_link 'Log Out'
     expect(page).to have_content 'You are logged out.'
     expect(page).to_not have_content 'Log Out'
