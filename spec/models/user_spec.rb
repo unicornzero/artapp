a0c340885cd08706acdbd726e5be25c60a26e85f
unicornzero <rylee.k@gmail.com>
2013-08-29 19:09:48 -0600

    validate password

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe 'User' do
+  describe 'validations' do
+    it 'should be valid with valid information' do
+      user = User.new(
+        email: 'user1@example.com',
+        password: 'password1',
+        password_confirmation: 'password1'
+        )
+      expect(user).to be_valid
+    end
+
+    its 'should not be valid with a confirmation mismatch' do
+      user = User.new(
+        email: 'user1@example.com',
+        password: 'password1',
+        password_confirmation: 'sssswwww1'
+        )
+      expect(user).to_not be_valid
+    end
+
+    it 'should not be valid without a password' do
+      user = User.new(
+        email: 'user1@example.com',
+        password: nil,
+        password_confirmation: nil
+        )
+      expect(user).to_not be_valid
+    end
+
+    it 'should not be valid with a short password' do
+      user = User.new(
+        email: 'user1@example.com',
+        password: 'cat',
+        password_confirmation: 'cat'
+        )
+      expect(user).to_not be_valid
+    end
+
+    its 'should be valid without a password' do
+      user = User.new(
+        email: 'user1@example.com',
+        password: nil,
+        password_confirmation: nil
+        )
+      expect(user).to_not be_valid
+    end
+  end
+end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 describe 'User' do
   it 'can create account' do
-    user = User.new
+    user = User.new(
+      email: 'user1@example.com',
+      password: 'password1',
+      password_confirmation: 'password1'
+      )
     expect(user).to be_valid
   end
-
-  it 'can login'
 end
