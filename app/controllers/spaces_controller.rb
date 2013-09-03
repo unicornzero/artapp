da3317f95c1efb684f26d90d74ba3b6f21e35f74
unicornzero <rylee.k@gmail.com>
2013-09-03 16:48:09 -0600

    logged-in user can create and edit Spaces

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,7 +1,38 @@
 class SpacesController < ApplicationController
+  before_filter :authorize, only: [:new, :create, :edit, :update]
+
+  def new
+    @space = Space.new
+  end
+
+  def create
+    @space = Space.new(params.require(:space).permit(:name))
+    if @space.save
+      flash[:success] = 'Your Space has been created.'
+      redirect_to @space
+    else
+      flash[:error] = 'Error- Your changes have not been saved'
+      render 'new'
+    end
+  end
 
   def show
     @space = Space.find(params[:id])
+  end
+
+  def edit
+    @space = Space.find(params[:id])
+  end
+
+  def update
+    @space = Space.find(params[:id])
+    if @space.update_attributes(params.require(:space).permit(:name))
+      flash[:success] = 'Your changes have been saved.'
+      redirect_to @space
+    else
+      flash[:error] = 'Error- Your changes have not been saved'
+      render 'edit'
+    end
   end
 
 end
--- spec/features/space_spec.rb
+++ spec/features/space_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Space' do
-  scenario 'guest can view', focus: true do
+  scenario 'guest can view' do
     space = create(:space)
     
     visit space_path(space)
@@ -9,14 +9,40 @@ feature 'Space' do
     expect(page).to have_content space.name
   end
 
+  scenario 'logged-in user can create' do
+    user = create(:user)
+    space = attributes_for(:space)
+    visit root_path
+    click_link 'Log In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log In'
+
+    visit new_space_path(space)
+    expect(page).to have_content 'Create a New Space'
+    fill_in 'Name', with: space[:name]
+    click_button 'Save'
+
+    expect(page).to have_content 'Your Space has been created'
+    expect(page).to have_content space[:name]
+  end
+
   scenario 'logged-in user can edit' do
     user = create(:user)
+    visit root_path
+    click_link 'Log In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log In'
     space = create(:space)
 
     visit edit_space_path(space)
+    expect(page).to have_content space.name
+    fill_in 'Name', with: 'Modified Name'
     click_button 'Save'
 
     expect(page).to have_content 'Your changes have been saved'
+    expect(page).to have_content 'Modified Name'
   end
 
   scenario 'logged-out user cannot cannot edit' do
@@ -24,7 +50,7 @@ feature 'Space' do
 
     visit edit_space_path(space)
 
-    expect(page).to have_content 'Unauthorized'
+    expect(page).to have_content 'Not Authorized'
   end
 
 end
