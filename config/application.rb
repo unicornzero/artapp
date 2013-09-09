873dfbc984f4fc89c06fa65935f95ad11bf79f0c
unicornzero <rylee.k@gmail.com>
2013-09-09 14:20:06 -0600

    configured config/application.yml to load CONFIG variables and development to send email via sftp

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'csv'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+CONFIG = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+CONFIG.merge! CONFIG.fetch(Rails.env, {})
+CONFIG.symbolize_keys!
+
 module Artapp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,12 +9,24 @@ Artapp::Application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Change mail delvery to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "test.unicornzero.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: CONFIG[:gmail_username],
+    password: CONFIG[:gmail_password]
+  }
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -26,4 +38,7 @@ Artapp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
