98f4a34b579a1e298610339bcf83229fa5653379
unicornzero <rylee.k@gmail.com>
2013-09-12 10:51:23 -0600

    refactored spec to validate email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,13 +8,18 @@ describe User do
     end
 
     it { should validate_presence_of(:email) }
-    xit { should validate_uniqueness_of(:email) } #pending https://github.com/thoughtbot/shoulda-matchers/commit/6336dd4987d0b6948e56dcf4259cfcb0c53f2666
     it { should_not allow_value("short").for(:password) }
     it { should_not allow_value("has spaces").for(:email) }
     it { should_not allow_value("incomplete").for(:email) }
     it { should_not allow_value("@.com").for(:email) }
     it { should_not allow_value("@example.com").for(:email) }
     it { should_not allow_value("a" * 100 + "@example.com").for(:email) }
+    
+    it 'validates uniqueness of email' do
+      user1 = create(:user, email: 'myemail@example.com' )
+      user2 = build(:user, email: 'myemail@example.com' )
+      expect(user2).not_to be_valid
+    end
 
     context 'password' do
       it 'should be present' do
