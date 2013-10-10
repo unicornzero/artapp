1cc2b92f3db37e721b6d8837ee14aaeb727e85c3
unicornzero <rylee.k@gmail.com>
2013-10-10 10:27:04 -0600

    unnecessary initializer removed

--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => CONFIG[:stripe_test_secret_key],
-  :secret_key      => CONFIG[:stripe_test_public_key]
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
