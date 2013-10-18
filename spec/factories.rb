4fc85e5aa7e4ced16e1047b448e9275bcc6b273e
unicornzero <rylee.k@gmail.com>
2013-10-18 13:36:00 -0600

    new owned_subscription factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,6 +6,34 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
     password 'password1'
-    password_confirmation 'password1'
+    password_confirmation { |u| u.password }
+  end
+  factory :subscription do
+    plan 'Basic'
+  end
+  factory :owned_space, class: Space do
+    name 'Awesome Gallery'
+    url 'http://www.awesomegallery.com'
+    user
+    association :subscription
+  end
+  factory :owned_subscription, class: Subscription do
+    plan 'Basic'
+    association :user
+    association :space
+    after(:build) do |sub|
+      sub.space.user_id = sub.user.id
+      sub.space.save
+    end
+  end
+  factory :pro_subscription, class: Subscription do
+    plan 'Pro'
+    stripe_cust_id 's_cust_123456'
+    association :user
+    association :space
+    after(:build) do |sub|
+      sub.space.user_id = sub.user.id
+      sub.space.save
+    end
   end
 end
