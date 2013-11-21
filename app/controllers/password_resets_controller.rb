5f6c090dc295ea9d6817f6cbe083fddee40d7476
unicornzero <rylee.k@gmail.com>
2013-11-21 10:43:25 -0700

    corrected typo in success message

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,7 +6,7 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     user.send_password_reset if user
-    redirect_to root_url, notice: 'Passwor d reset email sent '
+    redirect_to root_url, notice: 'Password reset email sent '
   end
 
   def edit
