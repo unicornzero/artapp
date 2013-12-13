5f25e86984b6170dc95c6e8cde2c8312e8e7e64b
unicornzero <rylee.k@gmail.com>
2013-12-13 11:37:20 -0700

    corrected typo in spec

--- spec/models/space_decorator_spec.rb
+++ spec/models/space_decorator_spec.rb
@@ -20,7 +20,7 @@ describe SpaceDecorator do
       expect(result).to eq "<a href=\"http://www.twitter.com/mytweets\">@mytweets</a>"     
     end
 
-    it 'returns a twitter link for handle "mytweets"' do
+    it 'returns a twitter link for handle "@mytweets"' do
       space.twitter = '@mytweets'
       result = SpaceDecorator.new(space).twitter_link
 
@@ -43,6 +43,4 @@ describe SpaceDecorator do
       expect(result).to eq "<a href=\"http://www.mywebsite.com\">www.mywebsite.com</a>"
     end
   end
-
-  pending 'handles absence of values'
 end
